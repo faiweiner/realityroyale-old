5908198bc45b8814f6a8e28b0b1fac4ccee3add5
faiweiner <fai.weiner@gmail.com>
2015-11-23 11:45:29 -0800

    added league types Fantasy Elimination to model

---
+++ app/models/elimination.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: leagues
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  season_id       :integer
+#  type            :string(255)
+#  participant_cap :integer
+#  draft_limit     :integer
+#  draft_date      :datetime
+#  draft_order     :string(255)
+#  league_key      :string(255)
+#  league_password :string(255)
+#  private         :boolean          default(FALSE), not null
+#  active          :boolean          default(FALSE), not null
+#  published       :boolean          default(FALSE), not null
+#  full            :boolean          default(FALSE), not null
+#  locked          :boolean          default(FALSE), not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
+class Elimination < League
+	def self.model_name
+		League.model_name
+	end
+end
