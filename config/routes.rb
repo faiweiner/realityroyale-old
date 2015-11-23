604cff9bf34bc6261fdd0b4a0c2d594f22bf9b55
faiweiner <fai.weiner@gmail.com>
2015-11-23 13:07:28 -0800

    Removes comments from the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,10 @@
-Rails.application.routes.draw do
+RrApi::Application.routes.draw do
 	devise_for :users
 	resources :contestants, except: [:new, :edit]
 	resources :episodes, except: [:new, :edit]
 	resources :leagues, except: [:new, :edit]
 	resources :seasons, except: [:new, :edit]
 	resources :shows, except: [:new, :edit]
-
-	# The priority is based upon order of creation: first created -> highest priority.
-	# See how all your routes lay out with "rake routes".
-
-	# You can have the root of your site routed with "root"
-	# root 'welcome#index'
-
-	# Example of regular route:
-	#   get 'products/:id' => 'catalog#view'
-
-	# Example of named route that can be invoked with purchase_url(id: product.id)
-	#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-	# Example resource route (maps HTTP verbs to controller actions automatically):
-	#   resources :products
-
-	# Example resource route with options:
-	#   resources :products do
-	#     member do
-	#       get 'short'
-	#       post 'toggle'
-	#     end
-	#
-	#     collection do
-	#       get 'sold'
-	#     end
-	#   end
-
-	# Example resource route with sub-resources:
-	#   resources :products do
-	#     resources :comments, :sales
-	#     resource :seller
-	#   end
-
-	# Example resource route with more complex sub-resources:
-	#   resources :products do
-	#     resources :comments
-	#     resources :sales do
-	#       get 'recent', on: :collection
-	#     end
-	#   end
-
-	# Example resource route with concerns:
-	#   concern :toggleable do
-	#     post 'toggle'
-	#   end
-	#   resources :posts, concerns: :toggleable
-	#   resources :photos, concerns: :toggleable
 
 	# Example resource route within a namespace:
 	#   namespace :admin do
